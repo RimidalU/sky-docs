478f8862c6d6a71fa9d59c5e771cd5906ef63e53
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-17 18:47:25 +0300

    feat: implement signin service

--- src/auth/constants/err.constants.ts
+++ src/auth/constants/err.constants.ts
@@ -1,5 +1,11 @@
 const INVALID_CREDENTIALS = 'Invalid credentials'
 const USER_EXISTS = 'User already exists'
+const USER_NOT_FOUND = 'User not found'
 const MISSING_ID_OR_PASSWORD = 'id and password required'
 
-export { INVALID_CREDENTIALS, USER_EXISTS, MISSING_ID_OR_PASSWORD }
+export {
+    INVALID_CREDENTIALS,
+    USER_EXISTS,
+    MISSING_ID_OR_PASSWORD,
+    USER_NOT_FOUND,
+}
---
+++ src/auth/services/signin.service.ts
@@ -0,0 +1,42 @@
+import { INVALID_CREDENTIALS } from '../constants/err.constants.js'
+import { findUserByIdentifier } from '../repositories/user.repository.js'
+import {
+    generateAccessToken,
+    generateRefreshToken,
+} from '../utils/token.utils.js'
+import { comparePassword } from '../utils/crypto.utils.js'
+import { saveRefreshToken } from '../repositories/refresh-token.repository.js'
+
+export interface TokenPair {
+    accessToken: string
+    refreshToken: string
+}
+
+const signinService = async (id: string, password: string) => {
+    try {
+        const user = await findUserByIdentifier(id)
+        if (!user) {
+            throw new Error(INVALID_CREDENTIALS)
+        }
+
+        const isPasswordValid = await comparePassword(password, user.password)
+        if (!isPasswordValid) {
+            throw new Error(INVALID_CREDENTIALS)
+        }
+
+        const accessToken = generateAccessToken(user.id)
+        const refreshToken = generateRefreshToken(user.id)
+
+        await saveRefreshToken({
+            token: refreshToken.token,
+            expiresAt: refreshToken.expiresAt,
+            user,
+        })
+
+        return { accessToken, refreshToken: refreshToken.token }
+    } catch (err) {
+        throw err
+    }
+}
+
+export { signinService }
