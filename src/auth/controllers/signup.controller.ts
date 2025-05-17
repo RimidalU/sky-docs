71fc999ba2af6cc752603ac6310b9ae244f2135d
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-17 15:58:01 +0300

    style: remove unused code

--- src/auth/controllers/signup.controller.ts
+++ src/auth/controllers/signup.controller.ts
@@ -5,13 +5,6 @@ import {
     USER_EXISTS,
 } from '../constants/err.constants.js'
 import { INTERNAL_SERVER_ERROR } from '../../constants/err.constants.js'
-import { createPasswordHash } from '../utils/crypto.utils.js'
-import { createUserIfNotExist } from '../repositories/user.repository.js'
-import {
-    generateAccessToken,
-    generateRefreshToken,
-} from '../utils/token.utils.js'
-import { saveRefreshToken } from '../repositories/refresh-token.repository.js'
 import { signupService } from '../services/signup.service.js'
 
 interface SignupRequest extends Request {
@@ -25,6 +18,7 @@ type SignupParams = {
     id: string
     password: string
 }
+
 const signupController = async (req: SignupRequest, res: Response) => {
     try {
         const { id, password } = req.body as SignupParams
