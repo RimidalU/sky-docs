8e1bc90b6a8a35ec6ddbea660ad2b3b6ab7e3569
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-17 14:53:53 +0300

    feat: implement signup controller

---
+++ src/auth/controllers/signup.controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express'
+import logger from '../../utils/logger.utils.js'
+import {
+    MISSING_ID_OR_PASSWORD,
+    USER_EXISTS,
+} from '../constants/err.constants.js'
+import { INTERNAL_SERVER_ERROR } from '../../constants/err.constants.js'
+import { createPasswordHash } from '../utils/crypto.utils.js'
+import { createUserIfNotExist } from '../repositories/user.repository.js'
+import {
+    generateAccessToken,
+    generateRefreshToken,
+} from '../utils/token.utils.js'
+import { saveRefreshToken } from '../repositories/refresh-token.repository.js'
+
+interface SignupRequest extends Request {
+    body: {
+        id: string
+        password: string
+    }
+}
+
+type SignupParams = {
+    id: string
+    password: string
+}
+const signupController = async (req: SignupRequest, res: Response) => {
+    try {
+        const { id, password } = req.body as SignupParams
+
+        if (!id || !password) {
+            return res.status(400).json({ error: MISSING_ID_OR_PASSWORD })
+        }
+
+        const hashedPassword = await createPasswordHash(password)
+
+        const user = await createUserIfNotExist(id, hashedPassword)
+
+        const accessToken = generateAccessToken(user.id)
+        const refreshToken = generateRefreshToken(user.id)
+
+        await saveRefreshToken({
+            token: refreshToken.token,
+            expiresAt: refreshToken.expiresAt,
+            user,
+        })
+
+        return res
+            .status(201)
+            .json({ accessToken, refreshToken: refreshToken.token })
+    } catch (err: any) {
+        logger.error('Signup Controller error:', err)
+
+        if (err.message === USER_EXISTS) {
+            return res.status(401).json({ error: USER_EXISTS })
+        }
+        return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
+    }
+}
+
+export { signupController }
