3e63f66fbb91eff0ff96af1ba8c33aa26ba8bd6c
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-17 20:54:44 +0300

    feat: implement info controller

---
+++ src/auth/controllers/info.controller.ts
@@ -0,0 +1,28 @@
+import { Response } from 'express'
+import logger from '../../utils/logger.utils.js'
+import { USER_NOT_FOUND } from '../constants/err.constants.js'
+import { INTERNAL_SERVER_ERROR } from '../../constants/err.constants.js'
+import { AuthRequest } from '../../types/common.types.js'
+import { infoService } from '../services/info.service.js'
+
+const infoController = async (req: AuthRequest, res: Response) => {
+    try {
+        const { userId } = req
+        if (!userId) {
+            return res.status(403).json({ message: USER_NOT_FOUND })
+        }
+
+        const identifier = await infoService(userId)
+
+        return res.status(201).json({ id: identifier })
+    } catch (err: any) {
+        if (err.message === USER_NOT_FOUND) {
+            return res.status(401).json({ error: USER_NOT_FOUND })
+        }
+        logger.error('InfoController Controller error:', err)
+
+        return res.status(500).json({ error: INTERNAL_SERVER_ERROR })
+    }
+}
+
+export { infoController }
--- src/auth/routes/info.route.ts
+++ src/auth/routes/info.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
+import { infoController } from '../controllers/info.controller.js'
+import { checkAuth } from '../../middleware/auth.middleware.js'
 
 const infoRouter = Router()
 
-infoRouter.get('/', () => {
-    // TODO: return user id
-})
+infoRouter.get('/', checkAuth, infoController)
 
 export { infoRouter }
