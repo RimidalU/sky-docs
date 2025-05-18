d9222704a5566edd41018f977434b49d6c81ca16
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-18 11:20:24 +0300

    feat: implement list-files service

--- src/file/constants/err.constants.ts
+++ src/file/constants/err.constants.ts
@@ -1,5 +1,11 @@
 const MISSING_FILE = 'File is required'
 const FILE_NOT_FOUND = 'File not found'
 const INVALID_FILE_ID = 'Invalid file id'
+const INVALID_PAGINATION_PARAMS = 'Invalid pagination parameters'
 
-export { MISSING_FILE, FILE_NOT_FOUND, INVALID_FILE_ID }
+export {
+    MISSING_FILE,
+    FILE_NOT_FOUND,
+    INVALID_FILE_ID,
+    INVALID_PAGINATION_PARAMS,
+}
