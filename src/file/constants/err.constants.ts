fdc67da7d3cf96bfcab24bd1b16f39eb781ac558
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-18 09:34:29 +0300

    feat: implement get-file-info service

---
+++ src/file/constants/err.constants.ts
@@ -0,0 +1,5 @@
+const MISSING_FILE = 'File is required'
+const FILE_NOT_FOUND = 'File not found'
+const INVALID_FILE_ID = 'Invalid file id'
+
+export { MISSING_FILE, FILE_NOT_FOUND, INVALID_FILE_ID }
