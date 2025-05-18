20d0e45906c8872b99da4307c190f9c31051ac6b
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-18 10:52:38 +0300

    feat: implement file name utils

--- src/file/utils/common.utils.ts
+++ src/file/utils/common.utils.ts
@@ -1,7 +1,52 @@
 import path from 'path'
+import fs from 'fs/promises'
+import logger from '../../utils/logger.utils.js'
 
-const getFileNameWithoutExt = (filename: string) => {
-    return path.parse(filename).name
+const UPLOAD_DIR = path.resolve('uploads')
+
+const deleteLocalFile = async (fileName: string): Promise<void> => {
+    const filePath = path.join(UPLOAD_DIR, fileName)
+
+    console.log(fileName)
+    console.log(filePath)
+
+    try {
+        await fs.unlink(filePath)
+    } catch (err: any) {
+        if (err.code === 'ENOENT') {
+            return
+        }
+        logger.error(`Failed to delete file ${fileName}:`, err)
+        throw err
+    }
 }
 
-export { getFileNameWithoutExt }
+const sanitizeFileName = (name: string): string => {
+    return name
+        .trim()
+        .replace(/\s+/g, '_')
+        .replace(/[^a-zA-Z0-9-_]/g, '')
+}
+
+const generateUniqueFileName = (baseName: string, ext: string): string => {
+    const timestamp = Date.now()
+    const randomSuffix = Math.random().toString(36).substring(2, 8)
+    return `${baseName}-${timestamp}-${randomSuffix}${ext}`
+}
+
+const getSanitizeFileNameAndExt = (name: string) => {
+    const parsed = path.parse(name)
+    const safeName = sanitizeFileName(parsed.name)
+    const ext = parsed.ext.toLowerCase()
+
+    return {
+        safeName,
+        ext,
+    }
+}
+export {
+    deleteLocalFile,
+    sanitizeFileName,
+    generateUniqueFileName,
+    getSanitizeFileNameAndExt,
+}
