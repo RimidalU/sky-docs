2f1c18cb18cd3ed21aa54fe532e7919272a8fed4
uladzimir <stankevichuuladimiru@gmail.com>
2025-05-19 14:09:43 +0300

    feat: add jti to refresh-tocken entityp

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -5,6 +5,7 @@ type StringRecord = Record<string, string>
 interface AuthRequest extends Request {
     userId?: number
     fingerprint?: string
+    jti?: string
 }
 
 export { StringRecord, AuthRequest }
